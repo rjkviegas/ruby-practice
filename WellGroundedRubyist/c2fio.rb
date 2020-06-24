2b486853380f680e75a4140290010f3eb7141b80
Ryan <rjkviegas@gmail.com>
2020-06-24 14:35:44 +0000

    Updated c2fio.rb so it will convert from Fahrenheit to Celsius

--- WellGroundedRubyist/c2fio.rb
+++ WellGroundedRubyist/c2fio.rb
@@ -1,8 +1,8 @@
-puts "Reading Celsius temperature value from data file..."
+puts "Reading Fahrenheit temperature value from data file..."
 num = File.read("temp.dat")
-celsius = num.to_i
-fahrenheit = (celsius * 9 / 5) + 32
+fahrenheit = num.to_i
+celsius = (fahrenheit - 32) * 5 / 9
 puts "Saving result to output file 'temp.out'"
 fh = File.new("temp.out", "w")
-fh.puts fahrenheit
+fh.puts celsius
 fh.close
