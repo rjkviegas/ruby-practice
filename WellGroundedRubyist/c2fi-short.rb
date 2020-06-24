f88fc0e29ab9c7eae99130d6886a50191f3f5fe3
Ryan <rjkviegas@gmail.com>
2020-06-24 14:01:18 +0000

    Added interactive version of c2f.rb program and also a shorter code version named c2fi-short.rb

---
+++ WellGroundedRubyist/c2fi-short.rb
@@ -0,0 +1,3 @@
+print "Hello. Please enter a Celsius value: "
+print "The Fahrenheit equivalent is ", gets.to_i * 9 / 5 + 32, ".\n"
+
--- WellGroundedRubyist/c2fi.rb
+++ WellGroundedRubyist/c2fi.rb
@@ -1,7 +1,7 @@
 print "Hello. Please enter a Celsius value: "
-celsius = 100
-fahrenheit = (celsius * 9 / 5) + 32
-print "The result is "
+celsius = gets
+fahrenheit = (celsius.to_i * 9 / 5) + 32
+print "The Fahrenheit equivalent is "
 print fahrenheit
 puts "."
 
