fde739a396650c2fe87c3ba9e1eaecd4731e2258
Ryan <rjkviegas@gmail.com>
2020-06-25 10:29:25 +0000

    Updated ticket.rb file with out commands

--- wellgroundedrubyist/chapter2/ticket.rb
+++ wellgroundedrubyist/chapter2/ticket.rb
@@ -18,3 +18,14 @@ end
 def ticket.price
   5.50
 end
+
+print "This ticket is for: "
+print ticket.event + ", at "
+print ticket.venue + ", on "
+puts ticket.date + "."
+print "The performer is "
+puts ticket.performer + "."
+print "The seat is "
+print ticket.seat + ", "
+print "and it costs $"
+puts "%.2f." % ticket.price
