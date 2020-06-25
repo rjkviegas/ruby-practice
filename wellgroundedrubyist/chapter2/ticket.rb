2c44e7a4cc084cc345a0a92192df66a2fa70b324
Ryan <rjkviegas@gmail.com>
2020-06-25 10:22:43 +0000

    Created chapter2 directory (within wellgroundedrubyist directory) and ticket.rb file

---
+++ wellgroundedrubyist/chapter2/ticket.rb
@@ -0,0 +1,20 @@
+ticket = Object.new
+
+def ticket.date
+  "1903-01-02"
+end
+def ticket.venue
+  "Town Hall"
+end
+def ticket.event
+  "Author's reading"
+end
+def ticket.performer
+  "Mark Twain"
+end
+def ticket.seat
+  "Second Balcony, row J, seat 12"
+end
+def ticket.price
+  5.50
+end
