0d13ada572ea0261b40188f31b94a1aac46be6e9
Ryan <rjkviegas@gmail.com>
2020-06-27 10:16:28 +0000

    Updated ticket.rb with setter methods including use of (=) in setter method name

--- wellgroundedrubyist/chapter3/ticket.rb
+++ wellgroundedrubyist/chapter3/ticket.rb
@@ -1,3 +1,4 @@
+=begin
 class Ticket
   def initialize(venue,date)
     @venue = venue
@@ -16,4 +17,24 @@ cc = Ticket.new("Convention Center", "2014-12-13")
 puts "We've created two tickets."
 puts "The first is for a #{th.venue} event on #{th.date}."
 puts "The second is for an event on #{cc.date} at #{cc.venue}."
+=end
+
+class Ticket
+  def initialize(venue,date)
+    @venue = venue
+    @date = date
+  end
+  def price=(amount)
+    @price = amount
+  end
+  def price
+    @price
+  end
+end
+
+ticket = Ticket.new("Town Hall", "2013-11-12")
+ticket.price=(63.00)
+puts "The ticket costs $#{"%.2f" % ticket.price}."
+ticket.price=(72.50)
+puts "Whoops -- it just went up. It now costs $#{"%.2f" % ticket.price}."
 
