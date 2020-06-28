41b1b65b108455e9a26f213f1d3b9ce57c846b5f
Ryan <rjkviegas@gmail.com>
2020-06-28 13:12:21 +0000

    Updated ticket-attr.rb with a singleton method ,most_expensive, defined on the class object Ticket

--- wellgroundedrubyist/chapter3/ticket-attr.rb
+++ wellgroundedrubyist/chapter3/ticket-attr.rb
@@ -6,3 +6,21 @@ class Ticket
     @date = date
   end
 end
+
+# The method most_expensive is defined directly on the class object Ticket in singleton-method style
+# A singleton method defined on a class object is commonly referred to as a class method
+# Idea of a class method is that messages are sent to the object that's the class rather than an instance of the class
+def Ticket.most_expensive(*tickets)
+  tickets.max_by(&:price)
+end
+
+th = Ticket.new("Town Hall","2013-11-12")
+cc = Ticket.new("Convention Center","2014-12-13")
+fg = Ticket.new("Fairgrounds","2015-10-11")
+th.price = 12.55
+cc.price = 10.00
+fg.price = 18.00
+
+
+highest = Ticket.most_expensive(th,cc,fg)
+puts "The highest-priced ticket is the one for #{highest.venue}."
