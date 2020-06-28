c634d5d7fc5ed912b9b44e6a3bbf3c4803f0ea68
Ryan <rjkviegas@gmail.com>
2020-06-28 13:46:33 +0000

    Updated ticket-attr.rb with VENUES constant, which limits instances of Ticket class to having certain venues

--- wellgroundedrubyist/chapter3/ticket-attr.rb
+++ wellgroundedrubyist/chapter3/ticket-attr.rb
@@ -1,8 +1,13 @@
 class Ticket
+  VENUES = ["Convention Center", "Fairgrounds", "Town Hall"]
   attr_reader :venue, :date
   attr_accessor :price
   def initialize(venue,date)
-    @venue = venue
+    if VENUES.include?(venue)
+      @venue = venue
+    else 
+      raise ArgumentError, "Unknown venue #{venue}"
+    end
     @date = date
   end
 end
@@ -14,6 +19,7 @@ def Ticket.most_expensive(*tickets)
   tickets.max_by(&:price)
 end
 
+=begin
 th = Ticket.new("Town Hall","2013-11-12")
 cc = Ticket.new("Convention Center","2014-12-13")
 fg = Ticket.new("Fairgrounds","2015-10-11")
@@ -24,3 +30,10 @@ fg.price = 18.00
 
 highest = Ticket.most_expensive(th,cc,fg)
 puts "The highest-priced ticket is the one for #{highest.venue}."
+=end
+
+puts "The venues are:"
+puts Ticket::VENUES
+# Adds another venue to the Ticket class's venue list
+Ticket::VENUES << "High School Gym"
+puts Ticket::VENUES
